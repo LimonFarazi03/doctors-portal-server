15da9ecf1eb31d09436a901dd6c6e64c9b16e619
MD Limon Farazi <abidislamabir@gmail.com>
2022-06-22 22:15:51 +0600

    booking post added

--- index.js
+++ index.js
@@ -8,30 +8,39 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.w0lxv.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
-    const database = client.db("doctors-portal").collection("services");
-    
-    app.get('/service',async(req,res)=>{
+
+    const serviceCollection = client.db("doctors-portal").collection("services");
+    const bookingCollection = client.db("doctors-portal").collection("bookings");
+
+    app.get("/service", async (req, res) => {
       const query = req.body;
-      const cursor = database.find(query);
+      const cursor = serviceCollection.find(query);
       const result = await cursor.toArray(cursor);
-      res.send(result)
+      res.send(result);
+    })
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      console.log(booking)
+      const query = {treatment: booking.treatment, date:booking.date,patientName:patientName}
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({success:true,result});
     })
 
-  }
-  finally{
+  } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
