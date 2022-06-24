b7244f4e31a3e2de29396f4926f3236fd2991ce2
MD Limon Farazi <abidislamabir@gmail.com>
2022-06-24 12:37:45 +0600

    booking api crated with dynamic user

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ async function run() {
       res.send(services);
     });
 
+    app.get('/booking', async(req,res)=>{
+      const patientEmail = req.query.patientEmail;
+      const query = {patientEmail:patientEmail};
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
       console.log(booking)
